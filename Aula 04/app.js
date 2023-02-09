2ce1706dc1856a9f76dd4466ad4e99491fe44264
Xulia <juliasoaresdealmeidaj@gmail.com>
2023-02-09 16:58:02 -0300

    Aula 04 - Funções

--- Aula 04/app.js
+++ Aula 04/app.js
@@ -4,3 +4,37 @@
 * Data: 09/02/23
 * Versão: 1.0
 ***********************************************************************/
+
+var tabuada = require('./modulo/tabuada.js');
+
+var readline = require('readline');
+
+var entradaDados = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+entradaDados.question('Digite o multiplicando: ', function (multiplicando) {
+    let valorMult = Number(String(multiplicando.replace(',', '.')));
+
+
+    entradaDados.question('Digite o valor máximo do multiplicador: ', function (maxMultiplicador) {
+        let valorMax =  Number(String(maxMultiplicador.replace(',', '.')));
+        let resultado;
+
+        if(multiplicando == '' || maxMultiplicador == '')
+            console.log('ERRO: Não é possível calcular se algum dado estiver vazio.');
+        else if(isNaN(multiplicando) || isNaN(maxMultiplicador))
+            console.log('ERRO: Digite apenas números.');
+        else{
+            // resultado = matematica.calculadora();
+            // if(resultado === false)
+            //     entradaDados.close();
+            // else
+            //     console.log(resultado);
+            //     entradaDados.close();
+        }
+
+    });
+
+});
--- Aula 04/modulo/tabuada.js
+++ Aula 04/modulo/tabuada.js
@@ -4,3 +4,41 @@
 * Data: 09/02/23
 * Versão: 1.0
 ***********************************************************************/
+
+//Função que retorna o processamento de uma tabuada qualquer até um contador qualquer.
+const calcularTabuada = function (multiplicando, maxMultiplicador) {
+    let tabuada = Number(String(multiplicando).replace(',', '.'));
+    let maxContador = Number(String(maxMultiplicador).replace(',', '.'));
+    let status = true;
+    let cont = 0;
+    let resultado;
+
+
+    if(tabuada == 0 || maxContador == 0)
+        status = false;
+    else if(isNaN(tabuada) || isNaN(maxContador))
+        status = false;
+    else{
+        // while(cont <= maxContador){
+        //     resultado =  tabuada * cont;
+        //     console.log(tabuada + ' X ' + cont + ' = ' + resultado);
+        //     cont += 1;
+        //     cont = cont + 1;
+            //cont++;
+
+            for(let cont = 0; cont <= maxContador; cont++){
+                resultado =  tabuada * cont;
+                console.log(tabuada + ' X ' + cont + ' = ' + resultado);
+            }
+
+        }
+
+        return status;
+    }
+    
+
+calcularTabuada(7,90)
+
+module.exports = {
+    calcularTabuada
+}
